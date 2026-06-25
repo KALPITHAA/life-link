512d5e312caacd2b1541f77bb294c2136ef0ba90
KALPITHAA <kalpithaasreeezhil@gmail.com>
2026-06-25 14:14:31 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// firebase.js
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Create services
+const auth = firebase.auth();
+const db = firebase.firestore();
